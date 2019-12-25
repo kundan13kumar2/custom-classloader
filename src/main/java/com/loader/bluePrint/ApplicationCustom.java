8c54ec9bf57edb729e9bf2ededf92ee4da0a213a
Kundan Kumar <wb-kundankum607732@antfin.com>
2019-12-25 18:29:29 +0530

    Initial commit

---
+++ src/main/java/com/loader/bluePrint/ApplicationCustom.java
@@ -0,0 +1,12 @@
+/**
+ * Alipay.com Inc.
+ * Copyright (c) 2004-2019 All Rights Reserved.
+ */
+package com.loader.bluePrint;
+
+/**
+ * @author Kundan Kumar
+ * @version $Id: ApplicationCustom.java, v 0.1 2019-12-25 6:29 PM Kundan Kumar Exp $$
+ */
+public interface ApplicationCustom {
+}
---
+++ src/main/java/com/loader/bluePrint/BootstrapCustom.java
@@ -0,0 +1,12 @@
+/**
+ * Alipay.com Inc.
+ * Copyright (c) 2004-2019 All Rights Reserved.
+ */
+package com.loader.bluePrint;
+
+/**
+ * @author Kundan Kumar
+ * @version $Id: BootstrapCustom.java, v 0.1 2019-12-25 6:28 PM Kundan Kumar Exp $$
+ */
+public interface BootstrapCustom {
+}
